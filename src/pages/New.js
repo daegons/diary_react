346edc2164bdb8d756c03712b3acdee0d1080e86
daegons <119998807+daegons@users.noreply.github.com>
2023-04-13 18:17:21 +0900

    Add files via upload

---
+++ src/pages/New.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import DiaryEditor from "../components/DiaryEditor";
+
+const New = () => {
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `New page`;
+  }, []);
+
+  return (
+    <div>
+      <DiaryEditor />
+    </div>
+  );
+};
+
+export default New;
